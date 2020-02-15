e2be5b093594ac95fde44e7dd229249816096cde
Russell McQueeney <russell@brex.com>
2020-02-15 01:32:59 -0800

    clean

--- src/d2dmain.cpp
+++ src/d2dmain.cpp
@@ -3,15 +3,10 @@
 #include <cstdlib>
 #include <thread>
 #include <chrono>
-#include <sstream>
-#include <mutex>
 
-#include <dlib/image_io.h>
-#include <dlib/geometry.h>
 #include <dlib/data_io.h>
 #include <dlib/image_processing.h>
 
-#include "surf_match.h"
 #include "map_matcher.h"
 
 #include "screenshot.h"
@@ -73,7 +68,6 @@ int WINAPI WinMain(
     s_w = (int)(fs_w_ratio * screenw),
     s_h = (int)(fs_h_ratio * screenh);
 
-  //const int s_x = 0, s_y = 0, s_w = 320, s_h = 320;
   std::vector<dlib::surf_point> map_surf_points;
   dlib::deserialize("s4launch_small.surf.dat") >> map_surf_points;
 
@@ -91,17 +85,13 @@ int WINAPI WinMain(
           {
             screenshot mss(capture, s_x, s_y, s_w, s_h);
             std::thread clock_pub([&app, &mss, &amp, &map_surf_points, &matcher, s_w, s_h] {
+                                    dlib::array2d<dlib::bgr_pixel> dimg;
+                                    dimg.set_size(s_h, s_w);
+
                                     for  (int i = 0; ; i++) {
-                                      std::cout  <<"Hello " <<i <<std::endl;
-                                      std::this_thread::sleep_for(0.2s);
-                                      std::cout  <<"Hello " <<i <<std::endl;
+                                      //std::this_thread::sleep_for(0.2s);
 
-
-                                      std::ostringstream oss;
-                                      oss <<"apx screenshot" <<i <<".png";
                                       mss.refresh();
-                                      dlib::array2d<dlib::bgr_pixel> dimg;
-                                      dimg.set_size(s_h, s_w);
                                       for (int r = 0; r < s_h; r++) {
                                         unsigned char *row = (unsigned char *)mss.get_bitmap_data() + (r * mss.width_step());
                                         for (int c = 0; c < s_w; c++) {
@@ -111,36 +101,11 @@ int WINAPI WinMain(
                                       }
 
                                       std::vector<dlib::surf_point> sub_surf_points = dlib::get_surf_points(dimg);
-                                      // std::vector<std::pair<size_t, size_t> > matched_points = match_surf_points(sub_surf_points, map_surf_points);
-                                      // std::vector<dlib::point> sub_points, map_points;
-                                      // for (auto it = begin(matched_points); it != end(matched_points); ++it) {
-                                      //   sub_points.push_back(sub_surf_points[it->first].p.center);
-                                      //   map_points.push_back(map_surf_points[it->second].p.center);
-                                      // }
-
-                                      // // if (i % 5 == 0) {
-                                      // //   dlib::save_png(dimg, oss.str());
-                                      // //   std::cout <<"Saved " <<oss.str() <<std::endl;
-                                      // // }
-
-                                      // if (sub_points.size() < 3) {
-                                      //   std::cout <<"Not enough matched points" <<std::endl;
-                                      //   continue;
-                                      // }
-                                      
-                                      // auto transform = dlib::find_affine_transform(sub_points, map_points);
-                                      // dlib::point sub_center(s_w/2, s_h/2);
-                                      // dlib::point new_position = transform(sub_center);
-                                      
-                                      // amp.x = new_position(0);
-                                      // amp.y = new_position(1);
-                                      
                                       auto r = matcher.find_match(sub_surf_points);
                                       amp.x = r.x;
                                       amp.y = r.y;
                                       
                                       SendMessage(app.get_hwnd(), message::AddMapPoint::message_type, (LPARAM)(&amp), 0);
-
                                     }
                                   });
 
